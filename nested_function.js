59519bee0781b24bf95523cdeeb240d003879f24
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-02-28 19:39:26 +0530

    Added nested_function

---
+++ nested_function.js
@@ -0,0 +1,16 @@
+function drive() {
+  // Find the car
+  function findTheCar() {
+    console.log("Find the car");
+  }
+
+  //Open the car
+  function openTheCar() {
+    console.log("Open the car");
+  }
+
+  findTheCar();
+  openTheCar();
+}
+
+drive();
