d4a3840859279af5a4ed0649d85091e6afd4bebb
Jayesh Tembhekar <jtembhekar2@gmail.com>
2020-03-25 20:06:49 +0530

    Added Javascript Classes

---
+++ classes.js
@@ -0,0 +1,11 @@
+//?      Javascript Classes
+// One way to define a class is using a class declaration.
+//  To declare a class, you use the class keyword with the name of the class
+//  ("Rectangle" here).
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
